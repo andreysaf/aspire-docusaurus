e075e5ee837d677cf10d66fc737a430f2d0e0a9c
Andrey Safonov <a.so777@live.ca>
2024-05-30 12:23:01 -0700

    Added search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,10 @@ const config = {
       }),
     ],
   ],
+  
+  plugins: [
+    require.resolve("@cmfcmf/docusaurus-search-local"),
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
